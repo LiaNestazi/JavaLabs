188a9cd2cf328cd7f97d96936b6027cc7fffbcf2
WatermelonSuga <56628545+SadSadGirl@users.noreply.github.com>
2020-11-24 13:33:56 +0300

    Add files via upload

---
+++ lab13/src/ru/mirea/lab13/Password.java
@@ -0,0 +1,16 @@
+package ru.mirea.lab13;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Password {
+    public static void main(String[] args) {
+        String password;
+        Scanner scan=new Scanner(System.in);
+        password=scan.nextLine();
+        Pattern p=Pattern.compile("(?=^\\w{8,}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).*$");
+        Matcher m=p.matcher(password);
+        System.out.println(m.matches());
+    }
+}
